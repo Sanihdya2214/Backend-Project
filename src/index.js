bb7e5bf8bde9df7ab232f9123c2aff5df025bab0
Sanidhya Agarwal <sanidhyaagarwal16@gmail.com>
2024-06-11 16:22:11 +0530

    App use creation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
 
+import { app
+    
+ } from "./app.js";
 dotenv.config({
     path:'./env'
 })
 
 connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+          console.log(`Server Is Running At Port: ${process.env.PORT || 8000}`)
+
+    })
+}
+)
+.catch((error)=>{
+    console.log("Database failed to Connect !!!",error)
+})
